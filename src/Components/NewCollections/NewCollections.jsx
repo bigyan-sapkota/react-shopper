db7093d962e52d0b09b79426060781091c26b3b6
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2023-12-16 22:41:12 +0545

    News letter section..

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./NewCollections.css";
+import new_collections from "../Assets/new_collections";
+import Item from "../Item/Item";
 
 const NewCollections = () => {
-  return <div className="newcollections"></div>;
+  return (
+    <div className="newcollections">
+      <h1>NEW COLLECTIONS</h1>
+      <img src={new_collections[0]} alt="" />
+      <hr />
+      <div className="collections">
+        {new_collections.map((item, i) => {
+          return (
+            <Item
+              key={i}
+              id={item.id}
+              name={item.name}
+              image={item.image}
+              new_price={item.new_price}
+              old_price={item.old_price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default NewCollections;

5c48dfe933c0d974a24b93217f48f30b77136376
bigyan-sapkota <bigyansapkotacodes@gmail.com>
2023-12-19 15:17:45 +0545

    Made Bread Crum and ProductDisplay pages

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./DescriptionBox.css";
+
+const DescriptionBox = () => {
+  return <div className="descriptionbox">DescriptionBox</div>;
+};
+
+export default DescriptionBox;
--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,8 +1,67 @@
 import React from "react";
 import "./ProductDisplay.css";
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
 
-const ProductDisplay = () => {
-  return <div className="productdisplay">ProductDisplay</div>;
+const ProductDisplay = (props) => {
+  const { product } = props;
+  return (
+    <div className="productdisplay">
+      <div className="productdisplay-left">
+        <div className="productdisplay-img-list">
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+        </div>
+        <div className="product-display-img">
+          <img className="productdisplay-main-img" src={product.image} alt="" />
+        </div>
+      </div>
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-stars">
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_dull_icon} alt="" />
+          <p>(122)</p>
+        </div>
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-price-old">
+            ${product.old_price}
+          </div>
+          <div className="productdisplay-right-price-new">
+            ${product.new_price}
+          </div>
+        </div>
+        <div className="productdisplay-right-description">
+          Elevate your wardrobe with our stylish shirt, offering a perfect blend
+          of contemporary design and timeless sophistication. Crafted with
+          precision and tailored for a flawless fit, it effortlessly transitions
+          from formal to casual occasions.
+        </div>
+        <div className="productdisplay-right-size">
+          <h1>Select Size</h1>
+          <div className="productdisplay-right-sizes">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+        <button>ADD TO CART</button>
+        <p className="productdisplay-right-category">
+          <span>Caetgory:</span> Women, T-Shirt, Crop Top
+        </p>
+        <p className="productdisplay-right-category">
+          <span>Tags:</span> Modern, Latest
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDisplay;
